654a77ada907e77e755b3c246b1055076ef0818f
Leonardo Castro <leonardofagundescastro@gmail.com>
2021-07-21 15:44:35 -0300

    adicionando endpoints de autor

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import winston from "winston";
 import clientesRouter from "./routes/cliente.route.js";
 //import livrosRouter from "./routes/livro.route.js";
-//import autoresRouter from "./routes/autor.route.js";
+import autoresRouter from "./routes/autor.route.js";
 //import vendasRouter from "./routes/venda.route.js";
 //import basicAuth from "express-basic-auth";
 //import {authorizer, authorize} from "./controllers/auth.controller.js";
@@ -34,6 +34,7 @@ app.use(cors());
 //app.use(basicAuth({ authorizeAsync: true, authorizer}));
 app.use("/cliente", clientesRouter);
 //app.use("/livro", livrosRouter);
+app.use("/autor", autoresRouter);
 //app.use("/autor", authorize("admin"), autoresRouter);
 //app.use("/venda", vendasRouter);
 app.use((err, req, res, next) => {
